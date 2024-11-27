4ee865bb4a2f1e118e6294a783bbcd17d257cc1a
Lukáš S. <stareclukas25@gmail.com>
2024-11-27 18:34:41 +0100

    update 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bakaweb-timetable",  
-    version="1.0.1",  
+    version="1.1",  
     description="Nástroj pro extrahování rozvrhu z Timetable modulu Bakawebu",
     long_description=long_description,
     long_description_content_type="text/markdown",
